f57971d25a51f5134744506e6ba0705dde9a1642
Arti2323 <123077179+Arti2323@users.noreply.github.com>
2023-04-19 15:32:19 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,22 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
 
-  return { min: min, max: max, avg: avg };
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] < min) {
+      min = arr[i];
+    }
+    if(arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  const avg = +(sum / arr.length).toFixed(2);
+  return { min, max, avg };
 }
+
 
 function summElementsWorker(...arr) {
 
